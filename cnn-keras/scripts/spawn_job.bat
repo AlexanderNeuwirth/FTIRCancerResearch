514dbb9aa74be8d82f94914b28823bbfa487fd32
Xander Neuwirth <neuwirtha@ad.msoe.edu>
2021-04-25 14:59:02 -0500

    C:\\dev\\FTIRCancerResearch\\cnn-keras\\scripts\\spawn_job.bat autojob

--- cnn-keras/scripts/spawn_job.bat
+++ cnn-keras/scripts/spawn_job.bat
@@ -26,7 +26,7 @@ SET DATETIME=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~1,2%%time:~3,2%%time:~6,2
 SET ID=%TARGET%_%DATETIME%
 
 :: Copy and fill in template script
-copy scripts\setup_template.sh scripts\temp.sh
+copy .\cnn-keras\scripts\setup_template.sh scripts\temp.sh
 powershell -Command "(gc scripts\temp.sh) -replace '{{BRANCH}}', '%TARGET%' | Out-File -encoding ASCII scripts\temp.sh"
 powershell -Command "(gc scripts\temp.sh) -replace '{{REMOTE}}', '%REMOTE%' | Out-File -encoding ASCII scripts\temp.sh"
 powershell -Command "(gc scripts\temp.sh) -replace '{{ID}}', '%ID%' | Out-File -encoding ASCII scripts\temp.sh"
