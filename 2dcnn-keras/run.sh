0af68107fc08b54af4fd9b7bff2a2e1c7a8aef61
Xander Neuwirth <neuwirtha@ad.msoe.edu>
2021-04-25 14:54:12 -0500

    test autojob

--- 2dcnn-keras/run.sh
+++ 2dcnn-keras/run.sh
@@ -7,6 +7,6 @@ samples=100000
 epochs=100
 batch=128
 
-python hsi_cnn_train_keras.py --data $data --masks $masks/train --crop $crop --checkpoint $checkpoint --classes $classes --epochs $epochs --batch $batch --balance --samples $samples --validate --valdata $data --valmasks $masks/val --valsamples $samples
+python hsi_cnn/hsi_cnn_train_keras.py --data $data --masks $masks/train --crop $crop --checkpoint $checkpoint --classes $classes --epochs $epochs --batch $batch --balance --samples $samples --validate --valdata $data --valmasks $masks/val --valsamples $samples
 
-python hsi_cnn_classify_keras.py --data $data --masks $masks/test --checkpoint $checkpoint --crop $crop --classes $classes
+python hsi_cnn/hsi_cnn_classify_keras.py --data $data --masks $masks/test --checkpoint $checkpoint --crop $crop --classes $classes
